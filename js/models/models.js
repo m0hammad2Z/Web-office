fda36f781f586fc790632a80130394a04effa8c7
iamrania <rania03shelbayeh@gmail.com>
2023-11-22 15:33:05 +0300

    Student LocalStorage by RANIA

--- js/models/models.js
+++ js/models/models.js
@@ -1,132 +1,216 @@
 import { general } from "../general.js";
 
-export function User(id, role, firstName, lastName, email, password, hiringDate, birthDate, imgURl, mobile){
-    this.id = id;
-    this.role = role;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
-    this.hiringDate = hiringDate;
-    this.birthDate = birthDate;
-    this.imgURl = imgURl;
-    this.mobile = mobile;
+export function User(
+  id,
+  role,
+  firstName,
+  lastName,
+  email,
+  password,
+  hiringDate,
+  birthDate,
+  imgURl,
+  mobile
+) {
+  this.id = id;
+  this.role = role;
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.email = email;
+  this.password = password;
+  this.hiringDate = hiringDate;
+  this.birthDate = birthDate;
+  this.imgURl = imgURl;
+  this.mobile = mobile;
 
-    this.add = function(){
-        general.users.push(this);
-        general.WriteOnlocalStorage(general.keysObj.users, JSON.stringify(general.users));
-    }
+  this.add = function () {
+    general.users.push(this);
+    general.WriteOnlocalStorage(
+      general.keysObj.users,
+      JSON.stringify(general.users)
+    );
+  };
 
-    this.delete = function(){
-        let index = general.users.findIndex(user => user.id == this.id);
-        general.users.splice(index, 1);
-        general.WriteOnlocalStorage(general.keysObj.users, JSON.stringify(general.users));
-    }
+  this.delete = function () {
+    let index = general.users.findIndex((user) => user.id == this.id);
+    general.users.splice(index, 1);
+    general.WriteOnlocalStorage(
+      general.keysObj.users,
+      JSON.stringify(general.users)
+    );
+  };
 
-    this.update = function(){
-        let index = general.users.findIndex(user => user.id == this.id);
-        general.users[index] = this;
-        general.WriteOnlocalStorage(general.keysObj.users, JSON.stringify(general.users));
-    }
+  this.update = function () {
+    let index = general.users.findIndex((user) => user.id == this.id);
+    general.users[index] = this;
+    general.WriteOnlocalStorage(
+      general.keysObj.users,
+      JSON.stringify(general.users)
+    );
+  };
 }
 
-export function Feedback(student_id, title, description, createdBy){
-    this.student_id = student_id;
-    this.title = title;
-    this.description = description;
-    this.createdBy = createdBy;
+export function Feedback(student_id, title, description, createdBy) {
+  this.student_id = student_id;
+  this.title = title;
+  this.description = description;
+  this.createdBy = createdBy;
 
-    this.add = function(){
-        general.feedbacks.push(this);
-        general.WriteOnlocalStorage(general.keysObj.feedbacks, JSON.stringify(general.feedbacks));
-    }
+  this.add = function () {
+    general.feedbacks.push(this);
+    general.WriteOnlocalStorage(
+      general.keysObj.feedbacks,
+      JSON.stringify(general.feedbacks)
+    );
+  };
 
-    this.delete = function(){
-        let index = general.feedbacks.findIndex(feedback => feedback.id == this.id);
-        general.feedbacks.splice(index, 1);
-        general.WriteOnlocalStorage(general.keysObj.feedbacks, JSON.stringify(general.feedbacks));
-    }
+  this.delete = function () {
+    let index = general.feedbacks.findIndex(
+      (feedback) => feedback.id == this.id
+    );
+    general.feedbacks.splice(index, 1);
+    general.WriteOnlocalStorage(
+      general.keysObj.feedbacks,
+      JSON.stringify(general.feedbacks)
+    );
+  };
 
-    this.update = function(){
-        let index = general.feedbacks.findIndex(feedback => feedback.id == this.id);
-        general.feedbacks[index] = this;
-        general.WriteOnlocalStorage(general.keysObj.feedbacks, JSON.stringify(general.feedbacks));
-    }
+  this.update = function () {
+    let index = general.feedbacks.findIndex(
+      (feedback) => feedback.id == this.id
+    );
+    general.feedbacks[index] = this;
+    general.WriteOnlocalStorage(
+      general.keysObj.feedbacks,
+      JSON.stringify(general.feedbacks)
+    );
+  };
 }
 
-export function Student(id, firstName, lastName, email, password, birthDate, team_leader_id, mobile, imgUrl){
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
-    this.birthDate = birthDate;
-    this.team_leader_id = team_leader_id;
-    this.mobile = mobile;
-    this.imgUrl = imgUrl;
+export function Student(
+  id,
+  firstName,
+  lastName,
+  email,
+  password,
+  birthDate,
+  team_leader_id,
+  mobile,
+  imgUrl,
+  rate,
+  absence,
+  tasks,
+  doneTasks,
+  feedbacks
+) {
+  this.id = id;
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.email = email;
+  this.password = password;
+  this.birthDate = birthDate;
+  this.team_leader_id = team_leader_id;
+  this.mobile = mobile;
+  this.imgUrl = imgUrl;
+  this.rate = rate;
+  this.absence = absence;
+  this.doneTasks = parseInt(doneTasks, 10) || 0;
+  this.tasks = tasks;
+  this.feedbacks = feedbacks;
 
-    this.add = function(){
-        general.students.push(this);
-        general.WriteOnlocalStorage(general.keysObj.students, JSON.stringify(general.students));
-    }
+  this.add = function () {
+    general.students.push(this);
+    general.WriteOnlocalStorage(
+      general.keysObj.students,
+      JSON.stringify(general.students)
+    );
+  };
 
-    this.delete = function(){
-        let index = general.students.findIndex(student => student.id == this.id);
-        general.students.splice(index, 1);
-        general.WriteOnlocalStorage(general.keysObj.students, JSON.stringify(general.students));
-    }
+  this.delete = function () {
+    let index = general.students.findIndex((student) => student.id == this.id);
+    general.students.splice(index, 1);
+    general.WriteOnlocalStorage(
+      general.keysObj.students,
+      JSON.stringify(general.students)
+    );
+  };
 
-    this.update = function(){
-        let index = general.students.findIndex(student => student.id == this.id);
-        general.students[index] = this;
-        general.WriteOnlocalStorage(general.keysObj.students, JSON.stringify(general.students));
-    }
+  this.update = function () {
+    let index = general.students.findIndex((student) => student.id == this.id);
+    general.students[index] = this;
+    general.WriteOnlocalStorage(
+      general.keysObj.students,
+      JSON.stringify(general.students)
+    );
+  };
 }
 
-export function Todo(students_ids, title, description, createdBy){
-    this.students_ids = students_ids;
-    this.title = title;
-    this.description = description;
-    this.createdBy = createdBy;
+export function Todo(students_ids, title, description, createdBy) {
+  this.students_ids = students_ids;
+  this.title = title;
+  this.description = description;
+  this.createdBy = createdBy;
 
-    this.add = function(){
-        general.todos.push(this);
-        general.WriteOnlocalStorage(general.keysObj.todos, JSON.stringify(general.todos));
-    }
+  this.add = function () {
+    general.todos.push(this);
+    general.WriteOnlocalStorage(
+      general.keysObj.todos,
+      JSON.stringify(general.todos)
+    );
+  };
 
-    this.delete = function(){
-        let index = general.todos.findIndex(todo => todo.id == this.id);
-        general.todos.splice(index, 1);
-        general.WriteOnlocalStorage(general.keysObj.todos, JSON.stringify(general.todos));
-    }
+  this.delete = function () {
+    let index = general.todos.findIndex((todo) => todo.id == this.id);
+    general.todos.splice(index, 1);
+    general.WriteOnlocalStorage(
+      general.keysObj.todos,
+      JSON.stringify(general.todos)
+    );
+  };
 
-    this.update = function(){
-        let index = general.todos.findIndex(todo => todo.id == this.id);
-        general.todos[index] = this;
-        general.WriteOnlocalStorage(general.keysObj.todos, JSON.stringify(general.todos));
-    }
+  this.update = function () {
+    let index = general.todos.findIndex((todo) => todo.id == this.id);
+    general.todos[index] = this;
+    general.WriteOnlocalStorage(
+      general.keysObj.todos,
+      JSON.stringify(general.todos)
+    );
+  };
 }
 
-export function Announcement(id, title, description, createdBy){
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.createdBy = createdBy;
+export function Announcement(id, title, description, createdBy) {
+  this.id = id;
+  this.title = title;
+  this.description = description;
+  this.createdBy = createdBy;
 
-    this.add = function(){
-        general.announcements.push(this);
-        general.WriteOnlocalStorage(general.keysObj.announcements, JSON.stringify(general.announcements));
-    }
+  this.add = function () {
+    general.announcements.push(this);
+    general.WriteOnlocalStorage(
+      general.keysObj.announcements,
+      JSON.stringify(general.announcements)
+    );
+  };
 
-    this.delete = function(){
-        let index = general.announcements.findIndex(announcement => announcement.id == this.id);
-        general.announcements.splice(index, 1);
-        general.WriteOnlocalStorage(general.keysObj.announcements, JSON.stringify(general.announcements));
-    }
+  this.delete = function () {
+    let index = general.announcements.findIndex(
+      (announcement) => announcement.id == this.id
+    );
+    general.announcements.splice(index, 1);
+    general.WriteOnlocalStorage(
+      general.keysObj.announcements,
+      JSON.stringify(general.announcements)
+    );
+  };
 
-    this.update = function(){
-        let index = general.announcements.findIndex(announcement => announcement.id == this.id);
-        general.announcements[index] = this;
-        general.WriteOnlocalStorage(general.keysObj.announcements, JSON.stringify(general.announcements));
-    }
+  this.update = function () {
+    let index = general.announcements.findIndex(
+      (announcement) => announcement.id == this.id
+    );
+    general.announcements[index] = this;
+    general.WriteOnlocalStorage(
+      general.keysObj.announcements,
+      JSON.stringify(general.announcements)
+    );
+  };
 }
---
+++ js/students.js
@@ -0,0 +1,195 @@
+import { Student } from "./models/models.js";
+loadStudentsIntoTable();
+document
+  .getElementById("addStudentForm")
+  .addEventListener("click", openPopupForm);
+document.getElementById("addStudentBtn").addEventListener("click", addStudent);
+document.getElementById("closeX").addEventListener("click", closePopupForm);
+function openPopupForm() {
+  var popup = document.getElementById("popupForm");
+  var overlay = document.getElementById("overlay");
+  popup.style.display = "block";
+  overlay.classList.add("active");
+}
+
+function closePopupForm() {
+  var popup = document.getElementById("popupForm");
+  var overlay = document.getElementById("overlay");
+  popup.style.display = "none";
+  overlay.classList.remove("active");
+  location.reload();
+}
+function addStudent() {
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var birthDate = document.getElementById("birthDate").value;
+  var mobile = document.getElementById("mobile").value;
+  var imgUrl = document.getElementById("imgUrl").value;
+  var registeredUser = JSON.parse(localStorage.getItem("registerd_user")) || {};
+  var teamLeaderId = registeredUser.id;
+
+  var newStudent = new Student(
+    Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1),
+    firstName,
+    lastName,
+    email,
+    password,
+    birthDate,
+    teamLeaderId,
+    mobile,
+    imgUrl,
+    0,
+    0,
+    0,
+    [],
+    []
+  );
+
+  if (!JSON.parse(localStorage.getItem("students"))) {
+    localStorage.setItem("students", JSON.stringify([]));
+  }
+
+  let students = JSON.parse(localStorage.getItem("students"));
+  students.push(newStudent);
+  localStorage.setItem("students", JSON.stringify(students));
+
+  closePopupForm();
+  loadStudentsIntoTable();
+}
+function loadStudentsIntoTable() {
+  const students = JSON.parse(localStorage.getItem("students")) || [];
+  var tableBody = document.querySelector("table tbody");
+  tableBody.innerHTML = "";
+  students.forEach(function (student) {
+    var row = tableBody.insertRow();
+    row.innerHTML = `
+    <td>${student.firstName} ${student.lastName}</td>
+    <td>${
+      student.rate ? student.rate : 0
+    }/100&nbsp; <button class="rateBtn plusBtn" data-student-id="${
+      student.id
+    }">+</button></td>
+    <td>${
+      student.absence ? student.absence : 0
+    }&nbsp;&nbsp; <button class="addAbsenceBtn plusBtn" data-student-id="${
+      student.id
+    }">+</button></td>
+    <td>${
+      student.doneTasks ? student.doneTasks : 0
+    }&nbsp;&nbsp; <button class="addDoneTaskBtn plusBtn" data-student-id="${
+      student.id
+    }">+</button></td>
+    <td>${student.tasks.length ? student.tasks : "No Tasks"}</td>
+    <td>${student.feedbacks.length ? student.feedbacks : "No Feedbacks"}</td>
+    <td><button class="editBtn" data-student-id="${
+      student.id
+    }">Edit</button></td>
+    <td><button class="deleteBtn" data-student-id="${
+      student.id
+    }">Delete</button>
+`;
+  });
+  document.querySelectorAll(".editBtn").forEach(function (editBtn) {
+    editBtn.addEventListener("click", function () {
+      var studentId = editBtn.getAttribute("data-student-id");
+      editStudent(studentId);
+    });
+  });
+
+  document.querySelectorAll(".deleteBtn").forEach(function (deleteBtn) {
+    deleteBtn.addEventListener("click", function () {
+      var studentId = deleteBtn.getAttribute("data-student-id");
+      deleteStudent(studentId);
+    });
+  });
+}
+function editStudent(studentId) {
+  var students = JSON.parse(localStorage.getItem("students")) || [];
+  var studentToEdit = students.find((student) => student.id === studentId);
+  document.getElementById("firstName").value = studentToEdit.firstName;
+  document.getElementById("lastName").value = studentToEdit.lastName;
+  document.getElementById("email").value = studentToEdit.email;
+  document.getElementById("password").value = studentToEdit.password;
+  document.getElementById("birthDate").value = studentToEdit.birthDate;
+  document.getElementById("mobile").value = studentToEdit.mobile;
+  document.getElementById("imgUrl").value = studentToEdit.imgUrl;
+  openPopupForm();
+  document
+    .getElementById("addStudentBtn")
+    .addEventListener("click", function () {
+      studentToEdit.firstName = document.getElementById("firstName").value;
+      studentToEdit.lastName = document.getElementById("lastName").value;
+      studentToEdit.email = document.getElementById("email").value;
+      studentToEdit.password = document.getElementById("password").value;
+      studentToEdit.birthDate = document.getElementById("birthDate").value;
+      studentToEdit.mobile = document.getElementById("mobile").value;
+      studentToEdit.imgUrl = document.getElementById("imgUrl").value;
+      localStorage.setItem("students", JSON.stringify(students));
+      closePopupForm();
+      location.reload();
+      loadStudentsIntoTable();
+    });
+}
+
+function deleteStudent(studentId) {
+  var students = JSON.parse(localStorage.getItem("students")) || [];
+  var studentIndex = students.findIndex((student) => student.id === studentId);
+  if (studentIndex !== -1) {
+    students.splice(studentIndex, 1);
+    localStorage.setItem("students", JSON.stringify(students));
+    loadStudentsIntoTable();
+  }
+}
+function addAbsence(studentId) {
+  var students = JSON.parse(localStorage.getItem("students")) || [];
+  var studentToUpdate = students.find((student) => student.id === studentId);
+  studentToUpdate.absence = (studentToUpdate.absence || 0) + 1;
+  localStorage.setItem("students", JSON.stringify(students));
+  loadStudentsIntoTable();
+}
+document
+  .querySelector("table tbody")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("addAbsenceBtn")) {
+      var studentId = event.target.getAttribute("data-student-id");
+      addAbsence(studentId);
+    }
+  });
+function addDoneTask(studentId) {
+  var students = JSON.parse(localStorage.getItem("students")) || [];
+  var studentToUpdate = students.find((student) => student.id === studentId);
+  if (studentToUpdate.tasks.length > studentToUpdate.doneTasks) {
+    studentToUpdate.doneTasks = (studentToUpdate.doneTasks || 0) + 1;
+    localStorage.setItem("students", JSON.stringify(students));
+    loadStudentsIntoTable();
+  }
+}
+
+document
+  .querySelector("table tbody")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("addDoneTaskBtn")) {
+      var studentId = event.target.getAttribute("data-student-id");
+      addDoneTask(studentId);
+    }
+  });
+
+document
+  .querySelector("table tbody")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("rateBtn")) {
+      var studentId = event.target.getAttribute("data-student-id");
+      addRate(studentId);
+    }
+  });
+function addRate(studentId) {
+  var students = JSON.parse(localStorage.getItem("students")) || [];
+  var studentToUpdate = students.find((student) => student.id === studentId);
+  studentToUpdate.rate = (studentToUpdate.rate || 0) + 5;
+  localStorage.setItem("students", JSON.stringify(students));
+  loadStudentsIntoTable();
+}
